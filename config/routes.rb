d0bb687e018cc90fc4b146ebd9a392d281eda057
3balljuggler <3balljuggling@gmail.com>
2020-11-07 10:57:49 +0900

    showページへのリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions'}
   root to: "enneagrams#index"
   resources :enneagrams, only: [:index, :new, :create, :update]
+  resources :users, only: [:show]
 end
