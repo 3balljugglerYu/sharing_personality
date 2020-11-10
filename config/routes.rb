903cc8fd2f8f259846f05755219bdbbbd5e25175
3balljuggler <3balljuggling@gmail.com>
2020-11-10 22:20:12 +0900

    再診断機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions'}
   root to: "enneagrams#index"
-  resources :enneagrams, only: [:index, :new, :create, :update]
+  resources :enneagrams, only: [:index, :new, :create, :edit, :update]
   resources :users, only: [:show]
 end
