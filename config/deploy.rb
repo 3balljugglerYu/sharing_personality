52b445fba1060cf5748514e1d3ae16e9116f2f11
3balljuggler <3balljuggling@gmail.com>
2020-12-19 10:56:10 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+# capistranoのバージョンを記載。
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
@@ -11,7 +11,7 @@ set :repo_url,  'git@github.com:3balljugglerYu/sharing_personality.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
