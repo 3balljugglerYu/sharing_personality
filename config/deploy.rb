45a964636083765f8cd3b0633adbdcd6db8098c5
3balljuggler <3balljuggling@gmail.com>
2020-12-19 11:12:39 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.14.1'
 
 # Capistranoのログの表示に利用する
 set :application, 'sharing_personality'
+set :deploy_to, '/var/www/sharing_personality'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:3balljugglerYu/sharing_personality.git'
